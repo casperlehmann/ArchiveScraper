b56f4b4dfd4759cf6f1eddbe977fd481b936b261
Laspimon <casperlehmann@gmail.com>
2016-04-05 16:51:19 +0200

    URL tests.

--- test/test.py
+++ test/test.py
@@ -8,8 +8,7 @@ class TestGetArchiveUrls(object):
 
     @classmethod
     def setup_class(cls):
-        cls.data = archiver.get_archive_urls(
-            from_date = '2016-04-01')
+        cls.data = archiver.get_archive_urls(from_date = '2016-04-01')
 
     @classmethod
     def teardown_class(cls):
@@ -41,3 +40,14 @@ class TestGetArchiveUrls(object):
             self.data[-1],
             'http://politics.people.com.cn/GB/70731/review/20120206.html')
 
+    def test_from_date_not_string(self):
+        assert_raises(
+            TypeError,
+            archiver.get_archive_urls,
+            20160401)
+
+    def test_from_date_string_wrong_len(self):
+        assert_raises(
+            ValueError,
+            archiver.get_archive_urls,
+            '2016-4-1')
