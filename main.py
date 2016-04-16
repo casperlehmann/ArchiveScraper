92ce7021ab1f9b052b8a3b282c3badca450e3157
Laspimon <casperlehmann@gmail.com>
2016-04-16 23:19:22 +0200

    Comment out.

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ if __name__ == '__main__':
         earliest_date='2012-02-06',
         schema = 'http://politics.people.com.cn/GB/70731/review/{}.html')
     dearch = archiver.Dearchiver()
-    #archive
 
     #dearch.clean()
     #dearch.load_data_files(silent = silent)
-    try:
-        dearch.load_archive(archive)
-    except httplib.IncompleteRead as e:
-        print (e.partial)
+    #try:
+    #    dearch.load_archive(archive)
+    #except httplib.IncompleteRead as e:
+    #    print (e.partial)
 
     #dearch.find_links_in_archive()
     #print( len(dearch.archive_meta))
