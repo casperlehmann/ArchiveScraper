e3668b877fab2046100b0b3a5b9a342ea7cba1ab
Laspimon <casperlehmann@gmail.com>
2016-04-24 18:20:42 +0200

    Update pipeline.

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from archiver import archiver, date_tools, url_tools
 # agent = archiver.Dearchiver()
 # url_tools.get_archive_urls() | 
 
+clean_archive = False
+scan_archive = False
+scan_articles = True
+
 if __name__ == '__main__':
     all_urls = url_tools.get_archive_urls(
         from_date = '2016-04-01',
@@ -13,19 +17,37 @@ if __name__ == '__main__':
         naming_json_file = '1_archive.json', scanned_json_file = '1_scanned.json',
         archive_folder = '1_archive')
 
-    #agent.clean()
-    #agent = archiver.Agent(
-    #    naming_json_file = 'archive.json', scanned_json_file = 'scanned.json')
+    if clean_archive:
+        agent.clean()
+        agent = archiver.Agent(
+            naming_json_file = '1_archive.json', scanned_json_file = '1_scanned.json',
+            archive_folder = '1_archive')
 
-    agent.load_archive(all_urls)
-    agent.find_links_in_archive(target_element = 'ul', target_class = 'list_16')
+    if scan_archive:
+        agent.load_archive(all_urls)
+        agent.find_links_in_archive(target_element = 'ul', target_class = 'list_16')
 
-    c = agent.count_links()
 
-    def stripper(href):
-        return href.strip().replace('GB/index.html','').strip('/')
+    if scan_articles:
+        home = 'http://politics.people.com.cn'
+        article_urls = set(
+            [item if not item.startswith('/') else home + item
+             for url, li in agent.scanned_file_data.items()
+             for item in li])
 
-    c = {href:count for href, count in c.items()
-                if not (stripper(href).split('.')[-1] in ['cn', 'com', ''])}
+        reader = archiver.Agent(
+            naming_json_file = '2_article.json', scanned_json_file = '2_artscan.json',
+            archive_folder = '2_articles')
+        print ('Fetching {} articles.'.format(len(article_urls)))
+        reader.load_archive(article_urls)
 
-    agent.show_counter(counter = c, root = 'http://politics.people.com.cn')
+    #for x in article_urls: print (x)
+    #c = agent.count_links()
+
+    #def stripper(href):
+    #    return href.strip().replace('GB/index.html','').strip('/')
+
+    #c = {href:count for href, count in c.items()
+    #            if not (stripper(href).split('.')[-1] in ['cn', 'com', ''])}
+
+    #agent.show_counter(counter = c, root = 'http://politics.people.com.cn')
