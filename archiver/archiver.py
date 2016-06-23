32ad8e2a8228f85f6754291a4ed800a5e3a6ccb0
Laspimon <casperlehmann@gmail.com>
2016-06-23 14:31:26 +0200

    Use logging module.

--- archiver/archiver.py
+++ archiver/archiver.py
@@ -8,12 +8,17 @@ import re
 import shutil
 import urllib.request
 import http.client
+import logging
 
 from collections import defaultdict as dd
 from socket import timeout
 from glob import glob
 
 from bs4 import BeautifulSoup as bs
+
+logging.basicConfig(
+    level = logging.DEBUG,
+    format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 class Agent(object):
     """Class for scraping website.
@@ -71,7 +76,7 @@ class Agent(object):
 
     def __init__(
             self, directory = None, naming_json_file = None,
-            scanned_json_file = None, silent = False, archive_folder = None):
+            scanned_json_file = None, archive_folder = None):
         self.directory = directory
         if not archive_folder is None:
             self._archive_folder = os.path.join(self.directory, archive_folder)
@@ -79,47 +84,42 @@ class Agent(object):
         self.naming_json_file = os.path.join(self.directory, naming_json_file)
 
         self.file_name_data = None
-        self.load_file_names_data_files(silent = silent)
+        self.load_file_names_data_files()
 
         self.scanned_json_file = os.path.join(self.directory, scanned_json_file)
-        self.load_scanned_file_data_files(silent = silent)
+        self.load_scanned_file_data_files()
 
-    def clean(self, silent = False):
+    def clean(self):
         """_"""
-        if not silent: print ('Cleaning...')
-        self.delete_file(target = self.naming_json_file, silent=silent)
-        self.delete_file(
-            target = os.path.join(self._directory, 'archive.json'),
-            silent=silent)
-        self.delete_file(
-            target = os.path.join(self._directory, 'scanned.json'),
-            silent=silent)
-        self.delete_file(target = self.naming_json_file, silent=silent)
-        self.clean_archive(silent=silent)
+        logging.info('Cleaning...')
+        self.delete_file(target = self.naming_json_file)
+        self.delete_file(target = os.path.join(self._directory, 'archive.json'))
+        self.delete_file(target = os.path.join(self._directory, 'scanned.json'))
+        self.delete_file(target = self.naming_json_file)
+        self.clean_archive()
         for f in glob(os.path.join(self.directory, '*')):
-            if not silent: print ('Deleting: ' + f)
+            logging.info('Deleting: %s\n', f)
             shutil.rmtree(f)
         self._archive_folder = None
         self.file_name_data = None
         self.scanned_file_data = None
         self._naming_json_file = None
         self._scanned_json_file = None
-        if not silent: print()
 
-    def clean_archive(self, silent = False):
+    def clean_archive(self):
         """_"""
         for f in glob(os.path.join(self._get_archive_folder(), '*')):
-            if not silent: print ('Deleting: ' + f)
+            logging.info('Deleting: ' + f)
             os.remove(f)
 
     @staticmethod
-    def delete_file(target, silent = False):
+    def delete_file(target):
         """_"""
         try:
-            if not silent: print ('Deleting: ' + target + '...')
+            logging.info('Deleting: ' + target + '...')
             os.remove(target)
         except FileNotFoundError:
-            if not silent: print ('Does not exist: ' + target)
+            logging.info('Does not exist: ' + target)
 
     def _get_filename(self, url):
         if not isinstance (url, str):
@@ -158,16 +158,15 @@ class Agent(object):
         return self._archive_folder
 
     # ScraperBase(object):
-    def load_file_names_data_files(self, silent = False):
+    def load_file_names_data_files(self):
         """_"""
-        if not silent: print ('Loading data files...')
+        logging.info('Loading data files...')
         try:
             self.file_name_data = dd(dict, json.load(open(self.naming_json_file)))
         except FileNotFoundError:
-            if not silent: print ('Creating new file:', self.naming_json_file)
+            logging.info('Creating new file: %s\n', self.naming_json_file)
             self.file_name_data = dd(dict)
             json.dump(self.file_name_data, open(self.naming_json_file, 'w'))
-        if not silent: print ()
 
     # File names and paths
     def _save_filename(self, url, fname):
@@ -179,7 +178,7 @@ class Agent(object):
         json.dump(self.file_name_data, open(self.naming_json_file, 'w'))
 
     # Data
-    def load_archive(self, urls, silent = False):
+    def load_archive(self, urls):
         """_"""
         four_o_fours = json.load(
             open(os.path.join(self._get_archive_folder(), '404.json')))
@@ -188,7 +187,7 @@ class Agent(object):
                 print ('404:    ', url, '(Previously checked)')
                 continue
             try:
-                self.load_archive_page(url, silent = silent)
+                self.load_archive_page(url)
             except urllib.error.HTTPError:
                 print ('404:    ', url)
                 four_o_fours.append(url)
@@ -205,22 +204,22 @@ class Agent(object):
                 continue
             except timeout:
                 print ('retry:  ', url)
-                self.load_archive_page(url, silent = silent)
+                self.load_archive_page(url)
 
-    def load_archive_page(self, url, silent = False):
+    def load_archive_page(self, url):
         """_"""
         if not isinstance (url, str):
             raise TypeError('url must be a string')
         try:
             fname = self._get_filename(url)
-            if not silent: print ('Alredy here: {}'.format(url))
+            logging.info('Alredy here: %s', url)
         except KeyError:
-            if not silent: print ('Fetching...: {}'.format(url))
-            self._fetch_archive_page(url, silent = silent)
+            logging.info('Fetching...: %s', url)
+            self._fetch_archive_page(url)
             fname = self._get_filename(url)
         return fname
 
-    def _fetch_archive_page(self, url, silent = False):
+    def _fetch_archive_page(self, url):
         if not isinstance(url, str):
             raise TypeError('url must be a string.')
         if not url.startswith('http'):
@@ -229,40 +228,39 @@ class Agent(object):
             fname = str(len(self.file_name_data)).zfill(6)
             fpath = os.path.join(self._get_archive_folder(), fname)
             with open(fpath, 'wb') as f:
-                if not silent: print ('Writing file: {}'.format(fname))
+                logging.info('Writing file: %s', fname)
                 f.write(url_obj.read())
                 self._save_filename(url, fname)
 
-    def load_article_pages(self, *urls, silent = False):
+    def load_article_pages(self, *urls):
         """_"""
         for url in urls:
             if url in self.file_name_data:
                 self._get_filename(url)
-                if not silent: print ('Alredy here')
+                logging.info('Alredy here')
             else:
-                self._fetch_article_page(url, silent = silent)
+                self._fetch_article_page(url)
                 self._get_filename(url)
 
-    def _fetch_article_page(self, url, silent = False):
+    def _fetch_article_page(self, url):
         with urllib.request.urlopen(url) as url_obj:
             os.makedirs(os.path.join(self.directory, 'articles'), exist_ok=True)
             fname = str(len(self.file_name_data)).zfill(6)
             with open(fname, 'wb') as f:
-                if not silent: print ('Writing file: {}'.format(fname))
+                logging.info('Writing file: %s', fname)
                 f.write(url_obj.read())
                 self._save_filename(url, fname)
 
     # ScannerBase(object):
-    def load_scanned_file_data_files(self, silent = False):
+    def load_scanned_file_data_files(self):
         """_"""
-        if not silent: print ('Loading data files...')
+        logging.info('Loading data files...')
         try:
             self.scanned_file_data = dd(dict, json.load(open(self.scanned_json_file)))
         except FileNotFoundError:
-            if not silent: print ('Creating new file:', self.scanned_json_file)
+            logging.info('Creating new file: %s\n', self.scanned_json_file)
             self.scanned_file_data = dd(dict)
             json.dump(self.scanned_file_data, open(self.scanned_json_file, 'w'))
-        if not silent: print ()
 
     def _save_links_from_page(self, url, links):
         if not isinstance (url, str):
@@ -272,15 +270,14 @@ class Agent(object):
         self.scanned_file_data[url] = links
         json.dump(self.scanned_file_data, open(self.scanned_json_file, 'w'))
 
-    def get_soup(self, fname, url = 'not supplied', silent = False):
+    def get_soup(self, fname, url = 'not supplied'):
         """_"""
         if fname is None or not isinstance(fname, str):
             raise TypeError("fname must be a string.")
         if url is None or not isinstance(url, str):
             raise TypeError("url must be a string.")
-        if not silent:
-            print ('Loading & Souping file: [{}] for url: [{}]'.format(
-                fname, url))
+        logging.info(
+            'Loading & Souping file: [%s] for url: [%s]', fname, url)
         try:
             fpath = os.path.join(self._get_archive_folder(), fname)
             with open(fpath, 'rb') as fobj:
@@ -289,7 +286,7 @@ class Agent(object):
             raise OSError('File not found: {}'.format(fname))
 
     def find_links_in_page(
-            self, url, silent = False,
+            self, url,
             target_element = None, target_class = None, target_id = None):
         """_"""
         if not isinstance(url, str): raise TypeError
@@ -304,7 +301,7 @@ class Agent(object):
         if not isinstance(target_id, str):
             raise TypeError('Parameter \'target_id\' must be a string.')
 
-        soup = self.get_soup(fname, silent = silent)
+        soup = self.get_soup(fname)
         target = soup.find(target_element, class_=target_class, id=target_id)
         if target is None: target = soup
         links = []
@@ -315,14 +312,12 @@ class Agent(object):
         self._save_links_from_page(url, links)
 
     def find_links_in_archive(
-            self, silent = False,
-            target_element = None, target_class = None, target_id = None):
+            self, target_element = None, target_class = None, target_id = None):
         """_"""
         for url in set(self.file_name_data.keys()):
             if not url in self.scanned_file_data:
                 self.find_links_in_page(
                     url,
-                    silent = silent,
                     target_element = target_element,
                     target_class = target_class,
                     target_id = target_id)
@@ -356,7 +351,7 @@ class Agent(object):
 
     # Feedback
     @staticmethod
-    def show_counter(counter, filtr = None, silent = False, root = None):
+    def show_counter(counter, root, filtr = None):
         """_"""
         if not isinstance(root, str):
             raise TypeError('Parameter \'root\' must be a string.')
@@ -374,6 +369,6 @@ class Agent(object):
             if stripped.endswith('.com') or stripped.endswith('.cn'):
                 continue
             if href.startswith('/'):
-                if not silent: print ('{:>8} {}'.format(count, root+href))
+                logging.info('%8s %s', count, root+href)
             else:
-                if not silent: print ('{:>8} {}'.format(count, href))
+                logging.info('%8s %s', count, href)
