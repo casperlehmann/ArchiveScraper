9858f1b6ef253ffe326ae4d4da693aeb03ca0569
Laspimon <casperlehmann@gmail.com>
2016-06-24 17:29:33 +0200

    Remove junk.

--- a.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logging.basicConfig(#filename='log.log',
-                    level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-logging.warning('AH! %10s end', '???')
-logging.info('lol')
